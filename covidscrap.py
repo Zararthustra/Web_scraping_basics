612c6ce8e7c7538bd9a47aace434ae49f35aa13f
Arthur <arthmayer@outlook.fr>
2021-01-20 00:04:21 +0000

    step 4

--- covidscrap.py
+++ covidscrap.py
@@ -5,9 +5,48 @@ url = "https://www.sante.fr/centres-vaccination-covid.html/"
 response = requests.get(url)
 
 if response.ok:
-    soup = BeautifulSoup(response.text)
-    span = soup.findAll('span')
-    name = soup.find('span', {'class': 'nom'})
+    web_page = BeautifulSoup(response.text, 'html.parser')
+    departments = web_page.findAll('span', class_='departement')
+    
+    for department in departments:
 
-    for n in span:
-        print(n.text + '\n')
+        centers = department.next_sibling.children
+        dep_len = len(department.text) + 2
+        center_num = 0
+        centers_num = len(list(department.next_sibling.children))
+        
+        print("{}\n {} \n{}\n\n Le département contient {} centres de vaccinations:\n"\
+                .format('='*dep_len, department.text, '='*dep_len, centers_num))
+
+        for center in centers:
+
+            center_num += 1
+
+            if center.find('span', class_='nom'):
+                name = center.find('span', class_='nom').text
+            else:
+                name = "Nom inconnu"
+            if center.find('span', class_='addresse'):
+                address = center.find('span', class_='addresse').text
+            else:
+                address = "Adresse inconnue"
+            if center.find('span', class_='codePostal'):
+                postal = center.find('span', class_='codePostal').text
+            else:
+                postal = "Code postal inconnu"
+            if center.find('span', class_='ville'):
+                city = center.find('span', class_='ville').text
+            else:
+                city = "Ville inconnue"
+            if center.find('span', class_='telephone fixe'):
+                phone = center.find('span', class_='telephone fixe').find('a').text
+            else:
+                phone = "Numero inconnu"
+            if center.find('span', class_='siteWeb'):
+                center_url = center.find('span', class_='siteWeb').find('a')['href']
+            else:
+                center_url = "Site web inconnu"
+
+            print("\t{})\t{}\n\nAdresse:\t{}\nCode Postal:\t{}\nVille:\t\t{}\nTéléphone:\t{}\
+                    \nSite web:\t{}\n\n".format(center_num, name, address, postal, city, phone, center_url))
+        
